1f8d716601d3e104f17e384276212e9caf41cc4d
ilovelili <route666@live.cn>
2018-05-29 01:45:25 +0900

    add k8s

--- micro-consul/main.go
+++ micro-consul/main.go
@@ -6,6 +6,7 @@ import (
 
 	hello "github.com/ilovelili/micro-consul/proto"
 	micro "github.com/micro/go-micro"
+	"github.com/micro/go-micro/registry"
 	// k8s "github.com/micro/kubernetes/go/micro"
 )
 
@@ -20,8 +21,14 @@ func (s *Say) Hello(ctx context.Context, req *hello.Request, rsp *hello.Response
 }
 
 func main() {
+	opts := registry.Option(func(opts *registry.Options) {
+		// opts.Addrs = []string{"10.27.253.238:8500"}   35.234.41.252:8500
+		opts.Addrs = []string{"35.234.41.252:8500"}
+	})
+
 	service := micro.NewService(
 		micro.Name("go.micro.api.greeter"),
+		micro.Registry(registry.NewRegistry(opts)),
 	)
 
 	service.Init()
